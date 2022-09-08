def0df01b17dd8e6dbb30a60c10e1b1db22cffcf
milonsarker <milon.sarker@gmail.com>
2022-09-08 12:33:59 -0500

    Gets data of Quotes, Author and tags

--- quotes_spider/quotes_spider/spiders/quotes.py
+++ quotes_spider/quotes_spider/spiders/quotes.py
@@ -2,11 +2,16 @@ import scrapy
 
 
 class QuotesSpider(scrapy.Spider):
-    name = 'quotes'
-    allowed_domains = ['quotes.toscrape.com']
-    start_urls = ['http://quotes.toscrape.com/']
+	name = 'quotes'
+	allowed_domains = ['quotes.toscrape.com']
+	start_urls = ['http://quotes.toscrape.com/']
 
-    def parse(self, response):
-	    h1_tag = response.xpath('//h1/a/text()').extract()
-	    tags = response.xpath('//*[@class="tag-item"]/a/text()').extract()
-	    yield {'H1': h1_tag, 'tags' : tags}
+	def parse(self, response):
+		quotes = response.xpath('//*[@class = "quote"]')
+		for qt in quotes: 
+			text = qt.xpath('.//*[@class = "text"]/text()').extract_first()
+			author = qt.xpath('.//*[@class = "author"]/text()').extract_first()
+			tags = qt.xpath('.//*[@itemprop = "keywords"]/@content').extract_first()
+			print("----------------------------------------------------------------------------")
+			print(text + "\n" + author + "\n" + str(tags))
+			print("----------------------------------------------------------------------------")
